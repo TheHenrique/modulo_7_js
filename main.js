8758a2e92196ef3a86f7bfbaac3f4066d655318a
Henrique Lustosa <65087187+TheHenrique@users.noreply.github.com>
2024-03-11 15:22:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form-deposito");
+  const campoA = document.getElementById("campo-A");
+  const campoB = document.getElementById("campo-B");
+  const errorMessage = document.querySelector(".error-message");
+  const successMessage = document.querySelector(".success-message");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    errorMessage.style.display = "none";
+    successMessage.style.display = "none";
+
+    if (campoA.value === "" || campoB.value === "") {
+      errorMessage.innerText = "Preencha este campo.";
+      errorMessage.style.display = "block";
+      return;
+    }
+
+    const numA = parseInt(campoA.value);
+    const numB = parseInt(campoB.value);
+
+    if (numB <= numA) {
+      errorMessage.innerText =
+        "O número do campo B deve ser maior que o número do campo A.";
+      errorMessage.style.display = "block";
+      return;
+    }
+
+    successMessage.innerText = "Parabéns! Está correto.";
+    successMessage.style.display = "block";
+  });
+
+  campoA.addEventListener("input", function () {
+    successMessage.style.display = "none";
+    errorMessage.style.display = "none";
+  });
+
+  campoB.addEventListener("input", function () {
+    successMessage.style.display = "none";
+    errorMessage.style.display = "none";
+  });
+});
